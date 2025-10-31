a602da5da79e77fd1b192711492b70fae7352ac9
200352303577 <it24102712@my.sliit.lk>
2025-10-31 14:21:09 +0530

    IT24102712  Lab 04

---
+++ Lab Sheet 04/IT24102712 PSLab4 .R
@@ -0,0 +1,30 @@
+setwd("C:\\Users\\IT24100644\\Desktop\\IT24100644")
+#1
+branch_data <- read.csv("Exercise.txt", header = TRUE)
+#2
+head(branch_data)
+str(branch_data)
+#3
+boxplot(branch_data$Sales,
+        main = "Boxplot of Sales",
+        ylab = "Sales",
+        col = "lightblue",
+        border = "black")
+#4
+fivenum(branch_data$Advertising)
+
+#5
+IQR(branch_data$Advertising)
+
+find_outliers <- function(x) {
+  Q1 <- quantile(x, 0.25)
+  Q3 <- quantile(x, 0.75)
+  IQR_val <- Q3 - Q1
+  outliers <- x[x < (Q1 - 1.5*IQR_val) | x > (Q3 + 1.5*IQR_val)]
+  return(outliers)
+}
+
+# Check outliers in Years variable
+find_outliers(branch_data$Years)
+
+
